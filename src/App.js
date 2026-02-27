9f33b63c70eac4c614b27f062243e7dd58ee1311
intissarboussaid <intissarboussaid2@gmail.com>
2026-02-27 16:53:10 +0100

    27-02-2026(03)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import useResizeScreen from "./utils/useResizeScreen";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+import { HashRouter, Routes, Route, Router } from 'react-router-dom';
 import HomeMediwave from "../src/components/mediwave/AppMediwave/App";
 import PageHomeVRme  from "../src/components/vrme/PageHomeVRme"
 import SteriwaveHome from "./components/steriwave/home/Home";
@@ -17,14 +17,14 @@ const App = () => {
 
   return (
     <div id="container">
-       <BrowserRouter basename="/mediwave">
+       <HashRouter basename="/mediwave">
         <Routes>
           <Route path="*" element={<HomeMediwave  />} />
           <Route path="vrme" element={<PageHomeVRme />} />
           <Route path="medilink" element={<AppMedilink />} />
           <Route path="steriwave" element={<SteriwaveHome />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
